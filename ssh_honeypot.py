578b5e546d91d65cdb126b8378aef824c6ce3aeb
ahlimany <ahliman44@outlook.com>
2025-09-19 18:26:31 +0400

    client handle

--- ssh_honeypot.py
+++ ssh_honeypot.py
@@ -3,9 +3,12 @@ import logging
 from logging.handlers import RotatingFileHandler
 import socket
 import paramiko
+import threading
 
 # Constants
 logging_format = "%(message)s"
+SSH_BANNER = "SSH-2.0-MySSHServer_1.0"
+host_key = paramiko.RSAKey(filename='server.key')
 
 # Loggers and Logging Files
 funnel_logger = logging.getLogger("FunnelLogger")
@@ -46,14 +49,15 @@ def emulated_shell(channel, clientip):
                 response = b'\n' + b'Go To deeboodah.com' + b'\r\n'
             else:
                 response = b'\n' + bytes(command.strip()) + b'\r\n'
-
-        channel.send(response)
-        channel.send(b'corporate-jumpbox2$ ')
-        command = b""
+                
+            channel.send(response)
+            channel.send(b'corporate-jumpbox2$ ')
+            command = b""
 
 # SSH Server and Sockets
-class SSHServer(paramiko.ServerInterface):
+class Server(paramiko.ServerInterface):
     def __init__(self, client_ip, input_username=None, input_password=None):
+        self.event = threading.Event()
         self.client_ip = client_ip
         self.input_username = input_username
         self.input_password = input_password
@@ -67,10 +71,12 @@ class SSHServer(paramiko.ServerInterface):
     
     def check_auth_password(self, username: str, password: str):
         if self.input_username is not None and self.input_password is not None:
-            if username == 'usename' and password == 'password':
+            if username == self.input_username and password == self.input_password  :
                 return paramiko.AUTH_SUCCESSFUL
             else:
                 return paramiko.AUTH_FAILED
+        else:
+            return paramiko.AUTH_SUCCESSFUL
     
     def check_channel_shell_request(self, channel):
         self.event.set()
@@ -83,4 +89,54 @@ class SSHServer(paramiko.ServerInterface):
         command = str(command)
         return True
 
+def client_handle(client, addr, username, password):
+    client_ip = addr[0]
+    print(f"{client_ip} has connected to the server.")
+
+    try:
+        transport = paramiko.Transport(client)
+        transport.local_version = SSH_BANNER
+        server = Server(client_ip=client_ip, input_username=username, input_password=password)
+
+        transport.add_server_key(host_key)
+        
+        transport.start_server(server=server)
+        
+        channel = transport.accept(100)
+        if channel is None:
+            print("No channel was opened.")
+        standard_banner = "Welcome to uBUNTU-22-04 ETS (Jemmy Jellyfish)!\r\n\r\n"
+        channel.send(standard_banner)
+        emulated_shell(channel, client_ip=client_handle)
+
+    except Exception as error:
+        print(error)
+        print("!!! Error !!!")
+        
+    finally:
+        try:
+            transport.close()
+        except Exception as error:
+            print(error)
+            print
+        client.close()
+
+
 # Provision SSH-based Honeypot
+def honeypot(address, port, username, password):
+    socks = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    socks.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    socks.bind(address, port)
+
+    socks.listen(100)
+    print(f"SSH Server is listening on port {port}.")
+
+    while True:
+        try:
+            client, addr = socks.accept()
+            ssh_honeypot_thread = threading.Thread(target=client_handle, args=(client, addr, username, password))
+            ssh_honeypot_thread.start()
+        except Exception as error:
+            print(error)
+
+honeypot('127.0.0.1', 2223, 'username', 'password')
